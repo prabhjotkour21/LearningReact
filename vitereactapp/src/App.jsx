721c3a4377d2e9ffd1701bdac83bf684264832ca
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-22 15:19:20 +0530

    first learning

---
+++ vitereactapp/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+   <h1>Hello World | Jot kour |  Today am learning React with vite  in this class  hitesh sir explain very well why we select vite insted of core react </h1>
+  )
+}
+
+export default App

9a3272d577616cffe9712c7098beca902d38268d
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-24 15:36:39 +0530

    feat: improve custom React implementation

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,24 +1,41 @@
 function customeRender(reactElement, container) {
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.Children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+    // container.appendChild(domElement)
+
+
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.Children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+    for (const i in reactElement.props) {
+        domElement.setAttribute(i,reactElement.props[i])
+    }
     container.appendChild(domElement)
 }
 
-const reactElement = {
-    type: 'a',
-    props: {
-        href: 'https://google.com',
-        target:'_blank'
-    },
-    Children: {
-        type: 'text',
-        
-    }
-}
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         // href: 'https://google.com',
+//         // target: '_blank',
+//         // id: 'btn1',
+//         // class: 'btn',
+//         // title:'google'
+//     },
+//     children: 'click me ||||'
+// }
 
-const maincontainer = document.querySelector('#root')
+// {text:prabhjot}
+const reactElement = {
+  type: "input",
+  props: {
+    type: "text",
+    value: "Prabhjot"
+  },
+  children: ""
+}
+// const maincontainer = document.querySelector('#root')
 
 customeRender(reactElement,maincontainer)
 

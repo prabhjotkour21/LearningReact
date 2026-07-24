02f0412f4ad053b49e8bea48f7d6ff6594a8ede7
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-24 15:36:46 +0530

    feat: update Vite React app components

--- vitereactapp/src/App.jsx
+++ vitereactapp/src/App.jsx
@@ -1,15 +1,19 @@
 
 import User from "./user.jsx"
 function App() {
-  
-
+  const username="jot kour"
+  // {username} esko evaluate exprection bolte h esko 
+  // 
   return (
 
     <>
-    <h1>Hello world</h1>
-   <User/>  
+    <h1>Hello world {username}</h1>
+   {/* <User/>   */}
     </>
 )
 }
+
+
+
 
 export default App
--- vitereactapp/src/main.jsx
+++ vitereactapp/src/main.jsx
@@ -1,10 +1,76 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Hi Jot Kour</h1>
+    </div>
+  )
+}
+
+//  MyApp m jo bhi likhte h jo end of the parse ki help se es code m convert hota 
+//  h or agr hum directly ye hi code ko  render m pass kr de to parser ka ek step bch jayega na 
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         // href: 'https://google.com',
+//         // target: '_blank',
+//         // id: 'btn1',
+//         // class: 'btn',
+//         // title:'google'
+//     },
+//     children: 'click me ||||'
+// }
+
+
+const element = React.createElement(
+  "a",
+  { href: "https://google.com" },
+  "Click Me"
 )
+
+console.log(element)
+
+const anotherReactElement = (
+  <a href="https://google.com" target="_blank"> click here </a>
+)
+const anotherUser = "vijay Singh"
+
+const ReactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', traget: '_blank' },
+  'Hey I am google',
+  anotherUser
+)
+// ye to ek object h  ye esi liye nhi chal rha kyu  react ek pareticuller object 
+// exptect krta h or ye object uske format m nhi h isiliye ye nhi chl rha
+
+createRoot(document.getElementById('root')).render(
+  
+    // {/* <MyApp /> */}
+  // MyApp()  ase bhi likh skte h kyu ki ye ek function hi to h
+  // anotherReactElement
+  ReactElement
+
+  // <App/>
+  
+)
+
+
+
+
+React.createElement(
+  "a",
+  null,
+
+  {
+    href:"https://google.com",
+    target:"_blank"
+  },
+  null,
+  "Google"
+ )

cc6b260505113dcdcf304d503256a38bd03f8d3b
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-25 17:53:15 +0530

    feat: update React application entry

--- counterapp/src/main.jsx
+++ counterapp/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import Scoreboard from "./Scoreboard.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Scoreboard/>
   </StrictMode>,
 )

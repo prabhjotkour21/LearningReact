8210912f5ab52233a9ea1b2f1300af95ebc269bd
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-27 10:08:10 +0530

    feat: add quiz application component

---
+++ counterapp/src/Quizapp.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Quizapp() {
+    return (
+        <>
+            <h3>Qus:React kis company ne banaya?</h3>
+            <select>
+                <option value="">Select</option>
+                <option value="Google">Google</option>
+                <option value="Meta">Meta</option>
+                <option value="Microsoft">Microsoft </option>
+            </select>
+            <h4>Ans:</h4>
+        </>
+    )
+}
+export default Quizapp

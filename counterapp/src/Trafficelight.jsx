46b31a34b2e69fa77991d5b1a3b4fe55df9f53ce
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-27 10:08:18 +0530

    feat: add traffic light component

---
+++ counterapp/src/Trafficelight.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function TrafficLight() {
+
+    const [light, setLight]= useState("Green")
+    return (
+        <>
+            
+            <h3>Current Light : { }</h3>
+            <button>Next</button>
+        </>
+    )
+}

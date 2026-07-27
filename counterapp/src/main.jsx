8c9974178451fce45ab6cffaf8e00018d09b0ed8
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-27 10:07:43 +0530

    feat: update React application entry

--- counterapp/src/main.jsx
+++ counterapp/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import Scoreboard from "./Scoreboard.jsx"
-
+import Quizapp from "./Quizapp.jsx"
+import Agechecker from "./Agecheacker.jsx"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Scoreboard/>
+    {/* <Scoreboard /> */}
+    {/* <Quizapp /> */}
+    <Agechecker/>
   </StrictMode>,
 )

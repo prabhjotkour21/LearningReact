f8ae22b134c021e1aba3c5f9e45fd03f32a1b7d4
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-07-29 15:58:30 +0530

    feat: enhance background changer functionality

--- backgroundChanger/src/App.jsx
+++ backgroundChanger/src/App.jsx
@@ -10,12 +10,12 @@ function App() {
   return (
     <div className='w-full h-screen duration-200'
      style={{backgroundColor:color}}
-    >  
+    >   <h1 className={`flex justify-center items-center ${ color === "white" ? "text-black" : "text-white" }`}>Current Color:{color.toUpperCase()}</h1>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'> 
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
 
         {arr.map((i)=>(  <button onClick={()=>setColor(i)} className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
-          style={{ backgroundColor: i }}>{i}</button>))}
+          style={{ backgroundColor: i }}>{i.toUpperCase()}</button>))}
 
         
           {/* <button onClick={()=>setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg '

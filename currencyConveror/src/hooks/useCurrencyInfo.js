4f5b9d8ea9978ca25e053bb940d02ad661043ffc
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-03 11:25:01 +0530

    feat: implement currency info hook

--- currencyConveror/src/hooks/useCurrencyInfo.js
+++ currencyConveror/src/hooks/useCurrencyInfo.js
@@ -1,20 +1,13 @@
-import { useState,useEffect } from "react";
-
-
+import { useState,useEffect } from "react"
 function useCurrencyInfo(currency) {
     const [data , setDate]=useState({})
-    useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json`)
-            .then((res) => res.json)
-            .then((res) => setDate(res[currency]))
+        useEffect(() => {
+            fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json`)
+                .then((res) => res.json)
+                .then((res) => setDate(res[currency]))
+            console.log(data);
+        },[currency])
         console.log(data);
-        
-    },[currency])
-
-
-    console.log(data);
     return data
-    
 }
-
 export default useCurrencyInfo

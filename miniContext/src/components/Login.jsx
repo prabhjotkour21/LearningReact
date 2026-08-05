98974c123eb40dc02e5f9de7c3e1a3d817a20f25
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-05 12:21:55 +0530

    feat: add login component

---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useContext } from 'react'
+import UserContext from '../context/UserContext'
+
+function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const{setUser} = useContext(UserContext)
+    const handleSubmmit = (e) => {
+        e.preventDefault()
+        setUser({username,password})
+    }
+  return (
+      <div>
+          <h2>Login</h2>
+          <input type='text' value={username} onChange={(i) => setUsername(i.target.value)} placeholder='username' />
+          {"  "}
+          <input type='text' value={password} onChange={(i) => setPassword(i.target.value)} placeholder='password' />
+          {"  "}
+           <input onClick={handleSubmmit} placeholder='Submit'/>
+    </div>
+  )
+}
+
+export default Login

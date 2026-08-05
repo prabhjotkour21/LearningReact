5fe8a76eb01bcd03ee87710b4a7292788a9de1d8
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-05 14:44:04 +0530

    refactor: update user context

--- miniContext/src/context/UserContext.js
+++ miniContext/src/context/UserContext.js
@@ -1,7 +1,11 @@
 import React from "react"
 
-
+// ye line ek context  object create krti  abhi es m koi data nhi h bad m jb UserContext.Provider k through is
+// context m value (user setuser) di jati   jis ko baki components access kr skte h 
+// React.createContext() ye sirf ek context obj bnata h ye data nhhi krta  data provider k value props through aata 
 const UserContext=React.createContext()
+
+
 
 
 export default UserContext

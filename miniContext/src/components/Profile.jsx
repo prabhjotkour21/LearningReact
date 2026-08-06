5c75c8109ab316b7b19f3ae26cec4bcd8d2e5fb8
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-06 11:59:50 +0530

    feat: enhance profile component

--- miniContext/src/components/Profile.jsx
+++ miniContext/src/components/Profile.jsx
@@ -1,9 +1,20 @@
 import React ,{useContext} from 'react'
 import UserContext from '../context/UserContext'
 function Profile() {
-   const{user} = useContext(UserContext)
-    if (!user) return <div>Please Login</div>
-    return <div> Welcome {user.username}</div>
+    const { user, isLoggedIn, logout } = useContext(UserContext)
+    if (!user) {
+        return <div>Please Login</div>
+    }
+    return (
+        <>
+            <div>WelCome { user.username}</div>
+            {/* if (!user) return <div>Please Login</div>
+            return <div> Welcome {user.username}{console.log("isLoggedIn", isLoggedIn)}</div> */}
+            <button onClick={logout}>Logout</button>
+  
+        </>
+       )
+          
 }
 
 export default Profile

3643b3b063df6dd76146e3a816f3f1e34613d765
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-06 11:59:56 +0530

    refactor: update user context provider

--- miniContext/src/context/UserContextProvider.jsx
+++ miniContext/src/context/UserContextProvider.jsx
@@ -5,10 +5,15 @@ import UserContext from "./UserContext";
 const UserContextProvider = ({children}) => {
     console.log(children);
     
-    const [user, setUser] =React.useState(null)
+    const [user, setUser] = React.useState(null)
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false)
+    const logout = () => {
+        setUser(null)
+        setIsLoggedIn(false)
+    }
     return (
         // or value jo h wo  aa rhi value ko context ko pass kr rha h 
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser ,isLoggedIn,setIsLoggedIn ,logout }}>
             {/* {props.children} */}
             {/* ye screen pr render krta h  */}
             {children}

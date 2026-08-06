bfbcf814810f857c268f2ddc18ecf42d533878e4
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-06 11:59:37 +0530

    feat: update mini context app

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import UserContextProvider from './context/UserContextProvider'
 import Login from './components/Login'
 import Profile from './components/Profile'
+import Status from "./components/Status"
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
     <UserContextProvider>
       <h1>React Context API Learning</h1> 
       <Login />
-      <Profile/>
+      <Profile />
+      <Status/>
     </UserContextProvider>
   )
 }

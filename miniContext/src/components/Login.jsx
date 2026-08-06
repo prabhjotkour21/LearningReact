26a37f402be3cce43a3d3be20af3528ce1a95a35
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-06 11:59:44 +0530

    feat: improve login component

--- miniContext/src/components/Login.jsx
+++ miniContext/src/components/Login.jsx
@@ -5,13 +5,18 @@ function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     
-    const{setUser} = useContext(UserContext)
+    const{user,setUser,setIsLoggedIn} = useContext(UserContext)
     const handleSubmmit = (e) => {
         e.preventDefault()
-        setUser({username,password})
+        setUser({ username, password })
+        setIsLoggedIn(true)
+    }
+    if (user) {
+        return null
     }
   return (
       <div>
+          
           <h2>Login</h2>
           <input type='text' value={username} onChange={(i) => setUsername(i.target.value)} placeholder='username' />
           {"  "}

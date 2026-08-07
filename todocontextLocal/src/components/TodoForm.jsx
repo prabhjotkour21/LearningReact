1cd1204a9bd6f48e60d105d604fdd8c12f2b1528
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-07 12:37:44 +0530

    feat: enhance todo form component

--- todocontextLocal/src/components/TodoForm.jsx
+++ todocontextLocal/src/components/TodoForm.jsx
@@ -10,7 +10,7 @@ function TodoForm() {
     const add = (e) => {
         e.preventDefault()
         if (!todo) return
-        addTodo({ todo, completed: false })
+        addTodo({ todo, completed: false ,createdAt:Date.now()})
         
         setTodo("")
     }

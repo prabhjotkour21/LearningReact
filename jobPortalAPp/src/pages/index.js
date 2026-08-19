b144fd510b07b65de42e013b9b61bca6942461cd
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-19 17:50:06 +0530

    chore: export page components

---
+++ jobPortalAPp/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home }from "./Home"
+export  {  default as About} from "./About"
+export  {  default as Jobs} from "./Jobs"
+export { default as JobDetails } from "./JobDetails"
+export { default as NotFound } from "./NotFound"
+export { default as Login } from "./Login"
+export {default as Dashboard} from "./Dashboard"
